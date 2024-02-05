1050d6e7374124ab506202e08eb716b0b4127609
safiahasan29 <safiahasan29@gmail.com>
2024-02-05 10:06:01 -0800

    created vpc project using terraform

--- 06-02-AWS-VPC-using-Terraform/terraform-manifests/v2-vpc-module-standardized/c1-versions.tf
+++ 06-02-AWS-VPC-using-Terraform/terraform-manifests/v2-vpc-module-standardized/c1-versions.tf
@@ -1,12 +1,14 @@
 # Terraform Block
 terraform {
-  #required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
-  required_version = ">= 1.0"   
+  # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  #required_version = ">= 1.0"
+  required_version = "~> 1.7"   
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       #version = ">= 4.65"      
-      version = ">= 5.31"      
+      #version = ">= 5.31"  
+      version = "~> 5.34"    
     }
   }
 }
